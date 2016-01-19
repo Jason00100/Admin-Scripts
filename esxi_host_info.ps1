95d4de943432382bcaff15bfbcbf54404b5221a8
Jason00100 <jpeckis@gmail.com>
2016-01-19 09:47:20 -0500

    esxi_host_info.ps1

---
+++ esxi_host_info.ps1
@@ -0,0 +1,6 @@
+Get-VMHost |Sort Name |Get-View |
+Select Name, Version
+@{N=“Type“;E={$_.Hardware.SystemInfo.Vendor+ “ “ + $_.Hardware.SystemInfo.Model}},
+@{N=“CPU“;E={“PROC:“ + $_.Hardware.CpuInfo.NumCpuPackages + “ CORES:“ + $_.Hardware.CpuInfo.NumCpuCores + “ MHZ: “ + [math]::round($_.Hardware.CpuInfo.Hz / 1000000, 0)}},
+@{N=“MEM“;E={“” + [math]::round($_.Hardware.MemorySize / 1GB, 0) + “ GB“}}
+#@{N="VER";E={$_.version}}
