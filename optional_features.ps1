1eacaa15f13eaddcb73c236ce0b65d6ae3f669a0
Jason00100 <jpeckis@gmail.com>
2016-01-19 11:57:28 -0500

    optional_features.ps1

---
+++ optional_features.ps1
@@ -0,0 +1,4 @@
+write-host -foregroundcolor yellow "This will find the ""Optional Features"" running on a remote host"
+$hN = Read-Host 'Enter host name'
+ gwmi win32_OptionalFeature -credential domain.local\username -computername $hn `
+ | Select-Object -Property caption, installstate | Where-Object {$_.installstate -eq 1} | sort-object caption
