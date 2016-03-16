c0108665eee2c8d8e94b4e179b465ff38b44e17d
Jason00100 <jpeckis@gmail.com>
2016-03-16 09:34:43 -0400

    Update DetectTimeDriftFromSource_V2.ps1

--- DetectTimeDriftFromSource_V2.ps1
+++ DetectTimeDriftFromSource_V2.ps1
@@ -2,7 +2,7 @@ $TimeSource = w32tm /query /status
 $Source = $TimeSource | ? {$_ -match 'Source:'} | % { ($_ -split ":\s\b")[1] }
  if ($source) { 
   $Source = $Source.Split(" ")[0]
-  $Source = $Source.Replace(",0x1","")
+#  $Source = $Source.Replace(",0x1","")
   }
 $Offset = w32tm /monitor /computers:$Source | FINDSTR NTP:
 $OSF = $Offset.Substring(9,11)
